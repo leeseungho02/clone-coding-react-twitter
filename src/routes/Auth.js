2e9abd67e28c6b544f30bfadeff8f86b329ada08
leeseungho02 <seungho020427@naver.com>
2020-11-02 15:32:12 +0900

    #2.1 Login Form part One

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Auth() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if(name === "email"){
+            setEmail(value);
+        } else if(name === "password"){
+            setPassword(value);
+        }
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
     return <div>
-        <span>Auth</span>
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="Email" name="email" value={email} required onChange={onChange} />
+            <input type="password" placeholder="Password" name="password" value={password} required onChange={onChange} />
+            <input type="submit" value="Log In" />
+        </form>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
     </div>;
 }
 
