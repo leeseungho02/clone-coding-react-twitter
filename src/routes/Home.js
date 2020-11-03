217b7fa91ad844ff1856e128e68d7106d03b85c1
leeseungho02 <seungho020427@naver.com>
2020-11-03 15:26:51 +0900

    #3.0 Form and Databse Setup & #3.1 Nweeting!

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,26 @@
+import { dbService } from 'fbase';
 import React from 'react';
 
 function Home() {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("nweets").add({
+            nweet: nweet,
+            createAt: Date.now()
+        });
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const { target: { value } } = event;
+        setNweet(value);
+    };
+
     return <div>
-        <span>Home</span>
+        <form onSubmit={onSubmit}>
+            <input type="text" value={nweet} placeholder="What's on you" onChange={onChange} />
+            <input type="submit" value="Nweet" />
+        </form>
     </div>;
 }
 
