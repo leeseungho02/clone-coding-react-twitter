becc0d5e3904759274165695e546998c58d1b2ea
leeseungho02 <seungho020427@naver.com>
2020-11-03 15:40:27 +0900

    #3.2 Getting the Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
 import { dbService } from 'fbase';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Home() {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            // ... => spread attribute 기능
+            const nweetObject = {
+                ...document.data(),
+                id: document.id
+            };
+            setNweets(prev => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
@@ -21,6 +36,13 @@ function Home() {
             <input type="text" value={nweet} placeholder="What's on you" onChange={onChange} />
             <input type="submit" value="Nweet" />
         </form>
+        <div>
+            {nweets.map(nweet => (
+                <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>
+            ))}
+        </div>
     </div>;
 }
 
