190da7d29ae0a77de8afa0dd5a9e27867a5b462f
leeseungho02 <seungho020427@naver.com>
2020-11-04 14:48:30 +0900

    #4.1 Preview Images part Two

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 function Home({ userObj }) {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     useEffect(() => {
         dbService.collection("nweets").onSnapshot((snapshot) => {
             // ... => spread attribute 기능
@@ -33,17 +34,26 @@ function Home({ userObj }) {
         const { target: { files } } = event;
         const file = files[0];
         const reader = new FileReader();
-        reader.onload = (result) => {
-            
+        reader.onload = (finishedEvent) => {
+            const { currentTarget: { result } } = finishedEvent;
+            setAttachment(result);
         }
         reader.readAsDataURL(file);
     };
+    const onClearAttachment = () => setAttachment(null);
 
     return <div>
         <form onSubmit={onSubmit}>
-            <input type="text" value={nweet} placeholder="What's on you mind?" onChange={onChange} />
+            <input type="text" value={nweet} placeholder="What's on you mind?" onChange={onChange} maxLength={120} />
             <input type="file" accept="image/*" onChange={onFileChange} />
             <input type="submit" value="Nweet" />
+            { attachment && (
+                <div>
+                <img src={attachment} width="50px" height="50px" />
+                <button onClick={onClearAttachment}>Clear</button>
+                </div>
+            )}
+            
         </form>
         <div>
             {nweets.map(nweet => (
